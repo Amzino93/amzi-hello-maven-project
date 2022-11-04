3a60f97c33150a110a502385c821b084453f8840
amza simpore <amzasimpore@gmail.com>
2022-11-03 23:42:10 -0500

    updating

---
+++ src/main/java/com/ibt/app/AppTest.java
@@ -0,0 +1,26 @@
+package com.ibt.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
+Footer
+        © 2022 GitHub, Inc.
+        Footer navigation
+        Terms
+        Privacy
+        S
