2a37e0070db862c9b6aa1dc1415ba1777f8375b6
amza simpore <amzasimpore@gmail.com>
2022-11-03 23:38:56 -0500

    updating

---
+++ src/main/java/com/ibt/app/App.java
@@ -0,0 +1,21 @@
+package com.ibt.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    String helloWorld() {
+        return "Hello World!";
+    }
+
+}
